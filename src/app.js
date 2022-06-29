10062a373c737ea649f7240e07a712336efd71ed
unknown <19ucc098@lnmiit.ac.in>
2022-06-30 02:54:59 +0530

    heroku1

--- src/app.js
+++ src/app.js
@@ -90,6 +90,6 @@ app.get('*',(req,res)=>{
     })
 })
 
-app.listen(port, () => {
-    console.log('Server is up on port ' + port)
-})
+app.listen(process.env.PORT || 3000, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+  });
